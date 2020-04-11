ff19e7f707c80cb7db04dea70432bc98a4cd32ec
PraveenRL <rlppraveen@gmail.com>
2020-04-11 20:32:57 +0530

    article

---
+++ db/migrate/20200411062554_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
+
+# rails db:migrate  ->  Create table in DB
